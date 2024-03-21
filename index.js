a51811fab456238a0359b33609894a52a399d0e4
USAMA MUSTAFA <hu95130@gmail.com>
2024-03-22 00:48:43 +0500

    my first project

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your First Number", type: "number", name: "firstNumber" },
+    {
+        message: "Enter your Second Number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Select valid operator");
+}
